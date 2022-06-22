bd5b62bca58bb55e204e3e9ff8233172ac84fde1
Luciana Zanetti <89159108+LulaZeta@users.noreply.github.com>
2022-06-22 16:11:24 -0300

    sort_listo (#31)

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllProducts } from '../../redux/actions/index';
+import { getAllProducts, ordenado } from '../../redux/actions/index';
 import SingleProduct from '../SingleProduct/SingleProduct';
 import Sort from '../Sort/Sort';
 import { Products_Container } from './Products.module.css';
+
 function Products() {
   const dispatch = useDispatch();
   const Products = useSelector((state) => state.Allproduct);
 
   useEffect(() => {
     dispatch(getAllProducts());
+    dispatch(ordenado());
   }, []);
   return (
     <div className={Products_Container}>
--- frontend/src/components/Sort/Sort.jsx
+++ frontend/src/components/Sort/Sort.jsx
@@ -1,25 +1,26 @@
-//import { ordenado } from '../../redux/actions';
-//import React, { useState, useEffect } from 'react';
+import { ordenado } from '../../redux/actions';
+import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
+import { useDispatch } from 'react-redux';
 
 export default function Sort() {
-  //   const [order, setOrder] = useState('');
-  //   const dispatch = useDispatch();
+  const [order, setOrder] = useState('');
+  const dispatch = useDispatch();
 
-  //   useEffect(() => {
-  //     dispatch(ordenado(order));
-  //     // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   }, [order]);
+  useEffect(() => {
+    dispatch(ordenado());
+  }, [dispatch]);
 
-  //   const handleChange = (e) => {
-  //     e.preventDefault();
-  //     setOrder(e.target.value);
-  //   };
+  const handleChange = (e) => {
+    e.preventDefault();
+    dispatch(ordenado(e.target.value));
+    setOrder(`Ordenado ${e.target.value}`);
+  };
 
   return (
     <Form.Select onChange={(e) => handleChange(e)}>
-      <option value="High to Low Price">mayor precio</option>
-      <option value="Low to High Price">menor precio</option>
+      {/* <option value="High to Low Price">mayor precio</option>
+        <option value="Low to High Price">menor precio</option> */}
       <option value="A-Z">A-Z</option>
       <option value="Z-A">Z-A</option>
       <option value="High to Low Rating">mejor valorados</option>
