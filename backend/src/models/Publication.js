b927b2ac8939a82451fded97bfc02d8829d95022
MonzonJoaquin <84410085+MonzonJoaquin@users.noreply.github.com>
2022-06-22 11:34:37 -0300

    Actualizacion del msg de error de max y min
    Correcion del msg error del atributo title, prop min y max length

--- backend/src/models/Publication.js
+++ backend/src/models/Publication.js
@@ -6,8 +6,8 @@ const publicationSchema = new Schema({
     type: String,
     required: [true, "Title is required"],
     validate: [validator.isAlphanumeric, "Title must have letters and numbers only"],
-    maxlength: [100, "A user name must have less or equal then 40 characters"],
-    minlength: [4, "A user name must have more or equal then 4 characters"],
+    maxlength: [100, "A title must have less or equal then 100 characters"],
+    minlength: [4, "A title must have more or equal then 4 characters"],
   },
   subtitle: {
     type: String,
