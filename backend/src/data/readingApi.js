8b5cd438414a6576f0a20ccb698f98a8706f2ccc
JuanCruzRausch <84107721+JuanCruzRausch@users.noreply.github.com>
2022-06-22 18:22:38 -0300

    data para front pulida

--- backend/src/data/readingApi.js
+++ backend/src/data/readingApi.js
@@ -17,22 +17,108 @@ const getData = async (cant) => {
 
       for (let i = 0; i < cant; i++) {
         let p = dataApi.data.results[i];
-        let product = {
-          name: p?.title,
-          price: p?.price,
-          promoPrice: p?.sale_price,
-          currency: p?.currency_id,
-          stock: p?.available_quantity,
-          totalSold: p?.sold_quantity,
-          buyingMode: p?.buying_mode,
-          listingType: p?.listing_type_id,
-          condition: p?.condition,
-          image: p?.thumbnail,
-          mercadopago: p?.accepts_mercadopago,
-          province: p?.address.state_name,
-          freeShipping: p?.shipping.free_shipping,
-          category: dataApiCat.data[j].name,
-        };
+        let product;
+        if (p?.seller.eshop?.nick_name) {
+          product = {
+            id: p?.id,
+            title: p?.title,
+            price: p?.price,
+            promoPrice: p?.sale_price,
+            currency: p?.currency_id,
+            seller: {
+              id: p?.seller.id,
+              name: p?.seller.eshop?.nick_name,
+              registration_date: p?.seller.registration_date,
+              tags: p?.seller.tags,
+              image: p?.seller.eshop?.eshop_logo_url,
+              reputation: {
+                status: p?.seller.seller_reputation.power_seller_status,
+                level: p?.seller.seller_reputation.level_id,
+              },
+              metrics: {
+                cancellations: {
+                  period:
+                    p?.seller.seller_reputation.metrics.cancellations.period,
+                  quantity:
+                    p?.seller.seller_reputation.metrics.cancellations.value,
+                },
+                claims: {
+                  period: p?.seller.seller_reputation.metrics.claims.period,
+                  quantity: p?.seller.seller_reputation.metrics.claims.value,
+                },
+                sales: {
+                  period: p?.seller.seller_reputation.metrics.sales.period,
+                  quantity: p?.seller.seller_reputation.metrics.sales.value,
+                },
+              },
+              transactionsTotal: {
+                total: p?.seller.seller_reputation.transactions.total,
+                canceled: p?.seller.seller_reputation.transactions.canceled,
+                completed: p?.seller.seller_reputation.transactions.completed,
+              },
+            },
+            stock: p?.available_quantity,
+            totalSold: p?.sold_quantity,
+            buyingMode: p?.buying_mode,
+            listingType: p?.listing_type_id,
+            condition: p?.condition,
+            image: p?.thumbnail,
+            mercadopago: p?.accepts_mercadopago,
+            province: p?.address.state_name,
+            freeShipping: p?.shipping.free_shipping,
+            category: dataApiCat.data[j].name,
+          };
+        } else {
+          product = {
+            id: p?.id,
+            title: p?.title,
+            price: p?.price,
+            promoPrice: p?.sale_price,
+            currency: p?.currency_id,
+            seller: {
+              id: p?.seller.id,
+              name: null,
+              registration_date: p?.seller.registration_date,
+              tags: p?.seller.tags,
+              image: null,
+              reputation: {
+                status: p?.seller.seller_reputation.power_seller_status,
+                level: p?.seller.seller_reputation.level_id,
+              },
+              metrics: {
+                cancellations: {
+                  period:
+                    p?.seller.seller_reputation.metrics.cancellations.period,
+                  quantity:
+                    p?.seller.seller_reputation.metrics.cancellations.value,
+                },
+                claims: {
+                  period: p?.seller.seller_reputation.metrics.claims.period,
+                  quantity: p?.seller.seller_reputation.metrics.claims.value,
+                },
+                sales: {
+                  period: p?.seller.seller_reputation.metrics.sales.period,
+                  quantity: p?.seller.seller_reputation.metrics.sales.value,
+                },
+              },
+              transactionsTotal: {
+                total: p?.seller.seller_reputation.transactions.total,
+                canceled: p?.seller.seller_reputation.transactions.canceled,
+                completed: p?.seller.seller_reputation.transactions.completed,
+              },
+            },
+            stock: p?.available_quantity,
+            totalSold: p?.sold_quantity,
+            buyingMode: p?.buying_mode,
+            listingType: p?.listing_type_id,
+            condition: p?.condition,
+            image: p?.thumbnail,
+            mercadopago: p?.accepts_mercadopago,
+            province: p?.address.state_name,
+            freeShipping: p?.shipping.free_shipping,
+            category: dataApiCat.data[j].name,
+          };
+        }
         prodArr.push(product);
       }
       if (j === 15) {
@@ -52,4 +138,4 @@ const getData = async (cant) => {
   }
 };
 
-getData(1);
+getData(20);
