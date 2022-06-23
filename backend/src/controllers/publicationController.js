800acef4c58f6179743ce934a72df4cfa660a961
MonzonJoaquin <84410085+MonzonJoaquin@users.noreply.github.com>
2022-06-23 10:39:13 -0300

    Update schema publication & user(/src/models/...)

--- backend/src/controllers/publicationController.js
+++ backend/src/controllers/publicationController.js
@@ -9,7 +9,6 @@ exports.post = async (req, res, next) => {
     const category = await Category.find({ name: publication.category });
     const newPublication = await Publication.create({
       title: publication.title,
-      subtitle: publication.subtitle,
       descriptions: publication.descriptions,
       price: publication.price,
       currency: publication.currency,
@@ -34,50 +33,3 @@ exports.post = async (req, res, next) => {
   }
 };
 
-
-/*
-  {
-    "id": "MLA694601563",
-    "title": "Matafuego 5kg Nuevo Abc Con Iram C Tarjeta + Soporte + Chapa",
-    "price": 9639,
-    "promoPrice": null,
-    "currency": "ARS",
-    "seller": {
-      "id": 40727839,
-      "name": "DINAMIC STORE",
-      "registration_date": "2007-10-17T00:07:27.000-04:00",
-      "tags": [
-        "normal",
-        "eshop",
-        "mshops",
-        "credits_profile",
-        "credits_priority_4",
-        "messages_as_seller"
-      ],
-      "image": "http://http2.mlstatic.com/eshops-logo/40727839v9b7c5b.png",
-      "reputation": { "status": "platinum", "level": "5_green" },
-      "metrics": {
-        "cancellations": { "period": "60 days", "quantity": 23 },
-        "claims": { "period": "60 days", "quantity": 73 },
-        "sales": { "period": "60 days" }
-      },
-      "transactionsTotal": {
-        "total": 53787,
-        "canceled": 2778,
-        "completed": 51009
-      }
-    },
-    "stock": 1,
-    "totalSold": 500,
-    "buyingMode": "buy_it_now",
-    "listingType": "gold_special",
-    "condition": "new",
-    "image": "http://http2.mlstatic.com/D_790370-MLA47131067576_082021-I.jpg",
-    "mercadopago": true,
-    "province": "Buenos Aires",
-    "freeShipping": true,
-    "category": "Accesorios para Vehículos"
-  },
-
-
-*/
