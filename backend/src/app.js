f9ae96681052a1d113baf01ac112c8ddd71f80bc
JuanCruzRausch <84107721+JuanCruzRausch@users.noreply.github.com>
2022-06-23 13:53:30 -0300

    creando el manejador de errores (./src/utils/appError.js , ./src/util/catchAsync.js , ./src/controllers/appErrorController.js)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,27 @@
 // comentando ando
-const express = require("express");
-const morgan = require("morgan");
-const userRouter = require("./routes/userRouter");
-const categoriesRouter = require('./routes/categoriesRouter')
-const publicationRouter = require('./routes/publicationRouter')
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalerrorHandler = require('./controllers/appErrorController');
+const userRouter = require('./routes/userRouter');
+const categoriesRouter = require('./routes/categoriesRouter');
+const publicationRouter = require('./routes/publicationRouter');
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use("/users", userRouter);
-app.use('/categories', categoriesRouter)
-app.use('/publication', publicationRouter)
+app.use('/users', userRouter);
+app.use('/categories', categoriesRouter);
+app.use('/publication', publicationRouter);
+
+app.all('*', (req, res, next) => {
+  //
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalerrorHandler);
 
 module.exports = app;
