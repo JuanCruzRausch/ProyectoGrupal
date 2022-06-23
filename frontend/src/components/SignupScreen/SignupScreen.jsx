e449aea671d13cd2e11469f12b51ace82f0084a2
Luciana Zanetti <89159108+LulaZeta@users.noreply.github.com>
2022-06-23 18:45:58 -0300

    SignIn/SignUp (#47)

---
+++ frontend/src/components/SignupScreen/SignupScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function SignupScreen() {
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInUrl ? redirectInUrl : '/';
+
+  return (
+    <Container className="small-container">
+      <h1 className="my-3">Sign Up</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control type="password" required />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Sign Up</Button>
+        </div>
+        <div className="mb-3">
+          Already have an account?{' '}
+          <Link to={`/signin?redirect=${redirect}`}>Sign-In</Link>
+        </div>
+      </Form>
+    </Container>
+  );
+}
