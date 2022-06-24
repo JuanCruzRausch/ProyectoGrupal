f34ee9da311b9831af84c65d42120bf52d9e0c6c
Javier Zapata <javierzv777@gmail.com>
2022-06-24 07:24:43 -0500

    funcionalidad de búsqueda solo con click en buscar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-<<<<<<< HEAD
-import { getProductByCategory, BuscarProducto, Navbarc, cartIMG } from "../../redux/actions"
-=======
 import { getProductByCategory, BuscarProducto, } from "../../redux/actions"
 import {Navbarc,cartIMG,DropdownA } from './Navbar.module.css'
->>>>>>> Dev
 import cart from '../../assets/img/cartICON.png'
-import { Navbarc, Autocomplete } from './Navbar.module.css'
 import { useState } from 'react'
 
 function NavbarComponent() {
@@ -43,53 +38,6 @@ function NavbarComponent() {
 
 
   return (
-<<<<<<< HEAD
-    <div>
-      <Navbar className={Navbarc} expand="lg" >
-        <Container fluid >
-          <Navbar.Brand href="#">Minimal Store</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-
-              navbarScroll
-            >
-              <NavDropdown title="Categorías" id="navbarScrollingDropdown"  >
-                {
-                  categories.map(category => {
-                    return (
-                      <NavDropdown.Item key={category.id} onClick={e => handleOnSelectCategory(e)} href="#">{category.name}</NavDropdown.Item>
-                    )
-                  })
-                }
-              </NavDropdown>
-              <Nav.Link href="/">Incio</Nav.Link>
-              <Nav.Link href="#action2">Mi perfil</Nav.Link>
-              <img className={cartIMG} src={cart} alt="cart" />
-            </Nav>
-            <Form className="d-flex" onSubmit={(e) => searchOnSubmit(e)}>
-              <FormControl
-                onClick={() => !search && setDisplayFlag(!displayFlag) && searchOnChange()}
-                type="search"
-                placeholder="buscar"
-                className="me-2"
-                aria-label="Search"
-                onChange={(e) => searchOnChange(e)}
-              />
-
-              <Button type="submit" variant="outline-success">Buscar</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* {displayFlag&&(
-        <div className={Autocomplete}>
-          {(display.map((e,i)=><NavDropdown.Item key={i}>{e}</NavDropdown.Item>)).filter((e,i)=>i<=5)}
-        </div>
-      )} */}
-    </div>
-=======
     <Navbar className={Navbarc} expand="lg" >
       <Container fluid >
         <Navbar.Brand href="/">Minimal Store</Navbar.Brand>
@@ -115,19 +63,18 @@ function NavbarComponent() {
             <Nav.Link href="#action2">Mi perfil</Nav.Link>
             <img className={cartIMG} src={cart} alt="cart"/>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex" onSubmit={(e) => searchOnSubmit(e)}>
             <FormControl
               type="search"
               placeholder="buscar"
               className="me-2"
               aria-label="Search"
-              onChange={(e) => dispatch(BuscarProducto(e.target.value))}/>
+              onChange={(e) => searchOnChange(e)} />
             <Button variant="outline-success">Buscar</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
->>>>>>> Dev
   )
 }
 
