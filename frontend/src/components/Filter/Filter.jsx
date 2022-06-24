8e267d1a463b73c2f8432b5a645c58d951b348dd
Javierzv777 <63484661+Javierzv777@users.noreply.github.com>
2022-06-24 08:19:05 -0500

    Feature search bar (#49)
    * first commit
    
    * funcionalidad de búsqueda solo con click en buscar
    
    * factorizando código extra mío inecesario que había comitiado
    
    * arreglando errores de renderizado

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,29 +1,30 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { subtitle, container } from './Filter.module.css'
 import { getProductByCategory } from '../../redux/actions'
+import { useState, useEffect } from 'react'
 // import { useEffect } from 'react'
 
 const Filter = (props) => {
-
-    let categories = useSelector(state => state.categories)
+    const [categories, setCategories] = useState([...(useSelector(state => state.categories)).map(category => {return {...category, count:0}})])
+    
     const dispatch = useDispatch()
     const products = useSelector(state=>state.Allproduct)
 
-    // useEffect(()=>{
-        
-    //     categories.forEach(category=>{
-    //         products.forEach((product, i) => {
-    //             if(product.category === category.name){
-    //                 categories[i].count++
-    //             }
-    //         });
-    //     })
-    // },[])
+    useEffect(()=>{
+        let productCategory = [...categories]
+        productCategory.forEach(category=>{
+            products.forEach((product, i) => {
+                if(product.category === category.name){
+                   category.count+=1 
+                }
+            });
+        })
+        setCategories([...productCategory])
+    },[])
 
     
     const handleOnSelectCategory = (e) => {
         e.preventDefault()
-        console.log(e.target.innerText)
         dispatch(getProductByCategory(e.target.innerText))
       }
     
@@ -37,11 +38,11 @@ const Filter = (props) => {
                     return (
                         <div key={category.id}>
                             <a onClick={(e)=>handleOnSelectCategory(e)} href='#'>
-                                {category.name}
+                                {category.name} 
                             </a>
-                            <span>
-                                {category.count}
-                            </span>
+                            <a onClick={(e)=>handleOnSelectCategory(e)} href="#">
+                                ({category.count})
+                            </a>
                         </div>
                     )
                 })}
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
-import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button  } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductByCategory, BuscarProducto, } from "../../redux/actions"
 import {Navbarc,cartIMG,DropdownA } from './Navbar.module.css'
 import cart from '../../assets/img/cartICON.png'
+import {useState} from "react"
+
 function NavbarComponent() {
-  
+
+  const [search, setSearch] = useState("")
   const dispatch = useDispatch()
-  const categories = useSelector(state=>state.categories)
+  const categories = useSelector(state => state.categories)
+
+
+
+  const searchOnSubmit = (e) => {
+    e.preventDefault()
+    console.log(search)
+    dispatch(BuscarProducto(search))
+  }
+
+  const searchOnChange = (e) => {
+    setSearch(e.target.value)
+  }
 
   const handleOnSelectCategory = (e) => {
     e.preventDefault()
-    console.log(e.target.innerText);
     dispatch(getProductByCategory(e.target.innerText))
   }
-  
+
 
   return (
     <Navbar className={Navbarc} expand="lg" >
@@ -42,13 +56,13 @@ function NavbarComponent() {
             <Nav.Link href="#action2">Mi perfil</Nav.Link>
             <img className={cartIMG} src={cart} alt="cart"/>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex" onSubmit={(e) => searchOnSubmit(e)}>
             <FormControl
               type="search"
               placeholder="buscar"
               className="me-2"
               aria-label="Search"
-              onChange={(e) => dispatch(BuscarProducto(e.target.value))}/>
+              onChange={(e) => searchOnChange(e)} />
             <Button variant="outline-success">Buscar</Button>
           </Form>
         </Navbar.Collapse>
