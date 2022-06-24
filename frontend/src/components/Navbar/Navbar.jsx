697681a7ef7727ce2647cedb431f0ff126a0ec0c
Javier Zapata <javierzv777@gmail.com>
2022-06-24 09:02:39 -0500

    type submit del botón

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -16,7 +16,6 @@ function NavbarComponent() {
 
   const searchOnSubmit = (e) => {
     e.preventDefault()
-    console.log(search)
     dispatch(BuscarProducto(search))
   }
 
@@ -63,7 +62,7 @@ function NavbarComponent() {
               className="me-2"
               aria-label="Search"
               onChange={(e) => searchOnChange(e)} />
-            <Button variant="outline-success">Buscar</Button>
+            <Button type="submit" variant="outline-success">Buscar</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
