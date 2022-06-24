473f90ee59cf2156b29be595ba9dcee4a7ba8389
JMBuccolini <96688216+JMBuccolini@users.noreply.github.com>
2022-06-24 13:21:23 -0300

    cors habilitado

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,14 +8,24 @@ const userRouter = require('./routes/userRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
 const publicationRouter = require('./routes/publicationRouter');
 
+
+
+
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use('/users', userRouter);
 app.use('/categories', categoriesRouter);
 app.use('/publication', publicationRouter);
+
 
 app.all('*', (req, res, next) => {
   //
