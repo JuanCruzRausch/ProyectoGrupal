1d43b2339d709f1cb629b084848903f0fca260a9
cristhian0512 <cristhian.pinedab@gmail.com>
2022-06-25 17:39:40 -0500

    ruta-user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,20 +1,20 @@
 const User = require('../models/User');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getUser= async (req, res, next) => {
-    try {
+exports.getUser= catchAsync(async (req, res, next) => {
+    
         const {idSeller} = req.params;
-        let sellerInfo = await User.find({_id:idSeller})
+        let sellerInfo = await User.findOne({_id:idSeller})
+
+        if(!sellerInfo) return next( new AppError('not found',400));
+
         res.status(201).json({
             status: "success",
             data: {
-              sellerInfo: sellerInfo,
+                sellerInfo
             },
           });
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            status: "fail",
-            message: error,
-        });
-    }
-}
+    
+        
+    });
