59e3d23e4c398c8a8dd202a970d0d1eb981294fe
Fede Carballo <carballo523@gmail.com>
2022-06-25 13:53:03 -0300

    paginado

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllProducts, ordenado } from '../../redux/actions/index';
+import { getAllProducts, getPaginate, ordenado } from '../../redux/actions/index';
 import SingleProduct from '../SingleProduct/SingleProduct';
 import Sort from '../Sort/Sort';
 import Filter from '../Filter/Filter'
 import { Products_Container, Cards_Container, Filter_Container, Cards_Filter_Container } from './Products.module.css';
 import Carousell from '../Carousel/Carousel';
+import Pagination from 'react-bootstrap/Pagination';
 
 function Products({ refElement, scrollTo }) {
   const dispatch = useDispatch();
   const productos = useSelector((state) => state.Allproduct);
- 
+  const [Active, setActive] = useState(1)
   useEffect(() => {
     dispatch(getAllProducts());
     dispatch(ordenado());
+    
   }, []);
+  let items = [];
+
+  for (let number = 1; number <= 10; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === Active} onClick={() =>handleGetProducts(number)}>
+      {number}
+    </Pagination.Item>,
+  );
+}
+const handleGetProducts = (pagina) =>{
+  dispatch(getPaginate(pagina)),
+  setActive(pagina)
+}
+
   return (
     <div className={Products_Container}>
       <Carousell />
@@ -34,6 +50,11 @@ function Products({ refElement, scrollTo }) {
           ))}
         </span>
       </div> 
+      <div>
+      <Pagination>
+        {items}
+        </Pagination>
+      </div>
     </div>
   );
 }
