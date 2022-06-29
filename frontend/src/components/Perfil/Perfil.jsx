dce72b43b676f5e3d83f859bfe87d211847e04a9
Fede Carballo <carballo523@gmail.com>
2022-06-29 17:41:30 -0300

    cambios a perfil, login y usuario

--- frontend/src/components/Perfil/Perfil.jsx
+++ frontend/src/components/Perfil/Perfil.jsx
@@ -37,7 +37,7 @@ export default function Perfil() {
     { isAuthenticated? (<div className={Container_card}>
           <div className={Container_Perfil}>
           <div className={Container_img_button}>
-          <img src="https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg" alt="perfil-img"/>
+          <img src={user.picture} alt="perfil-img"/>
           <Link to="/perfil/editar">
               <button>
               Editar perfil
