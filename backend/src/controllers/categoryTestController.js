0e3ef36a53fe13a75511cf0c375ba2e22f4c8a83
JMBuccolini <96688216+JMBuccolini@users.noreply.github.com>
2022-06-30 19:34:50 -0300

    get a categorias nuevas

--- backend/src/controllers/categoryTestController.js
+++ backend/src/controllers/categoryTestController.js
@@ -1,4 +1,5 @@
 const CategoryTest = require ('../models/CategoryTest');
+const SubCategory = require('../models/SubCategory');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -9,3 +10,17 @@ exports.post = catchAsync(async(req,res,next)=>{
     })
     res.status(201).json({status:"success", data: newCategory})
 });
+
+exports.getAllCategoryTest = (req,res)=>{
+    try {
+        CategoryTest.find({}, function(err,categories){
+            SubCategory.populate(categories, {path:"subcategories"}, function(err,categories){
+                res.status(200).send(categories)
+            })});
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+
+};
--- backend/src/routes/categoryTestRouter.js
+++ backend/src/routes/categoryTestRouter.js
@@ -4,6 +4,6 @@ const categoryTestController = require("../controllers/categoryTestController");
 const categoryTestRouter = express.Router();
 
 categoryTestRouter.post('/', categoryTestController.post);
-
+categoryTestRouter.get('/', categoryTestController.getAllCategoryTest)
 
 module.exports = categoryTestRouter;
