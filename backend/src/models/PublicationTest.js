2befd2e62e2f00c6096f8dae11fd771541a5f48d
JuanCruzRausch <84107721+JuanCruzRausch@users.noreply.github.com>
2022-06-29 21:00:03 -0300

    modelo publÃication3.0

--- backend/src/models/PublicationTest.js
+++ backend/src/models/PublicationTest.js
@@ -1,7 +1,37 @@
 const { Schema, model } = require('mongoose');
 const validator = require('validator');
 
-const publicationTestSchema = new Schema({});
+const publicationTestSchema = new Schema({
+  title: {},
+  description: {},
+  pictures: {},
+  price: {},
+  promPrice: {},
+  currency: {},
+  status: {},
+  seller: {},
+  category: {},
+  subCategory: {},
+  sellerShipping: {},
+  freeShipping: {},
+  pickup: {},
+  condition: {},
+  date_created: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  last_update: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  stock: {},
+  totalSold: {},
+  brand: {},
+  location: {},
+  visibility: {},
+  questions: {},
+});
 
 const PublicationTest = model('PublicationTest', publicationTestSchema);
 
