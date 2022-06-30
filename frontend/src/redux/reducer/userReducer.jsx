bf3d2b95343040417d83a922b0e8e630a2873dbd
Javierzv777 <63484661+Javierzv777@users.noreply.github.com>
2022-06-29 21:09:53 -0500

    cambié nombre del redux index por products (#114)

---
+++ frontend/src/redux/reducer/userReducer.jsx
@@ -0,0 +1,17 @@
+import {} from '../actions/userAction.jsx'
+
+
+
+const initialState={
+    user:{
+
+    }
+};
+
+function userReducer (state = initialState, { type, payload}) {
+    switch(type){
+        default: return state
+    }
+};
+
+export default userReducer;
