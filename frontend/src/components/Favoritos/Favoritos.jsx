333521bca821ac442d4ec32a368331f64bb7fd73
Fede Carballo <carballo523@gmail.com>
2022-07-01 12:06:07 -0300

    fix favs

--- frontend/src/components/Favoritos/Favoritos.jsx
+++ frontend/src/components/Favoritos/Favoritos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import {FavContainer, FavDiv} from './Favoritos.module.css'
+import {ItemsContainer_SingleItem, EliminarItem} from '../Cart/Cart.module.css'
 function Favoritos() {
     JSON.parse(localStorage.getItem('favs'));
     const FavState = useSelector(state=> state.FavReducer.Favs)
@@ -11,10 +12,11 @@ function Favoritos() {
            <hr />
            <div>
                 {
-                FavState.map((e) => 
-                <div>
+                FavState.map((e,i) => 
+                <div className={ItemsContainer_SingleItem}  key={i}>
+                    <img src ={e.thumbnail} alt={e.title}/>
                     <h1>{e.title}</h1>
-                    <h2>{e.price}</h2>
+                    <h2>${e.price}</h2>
                     <h2>{e.province}</h2>
                     {
                     e.freeShipping == true ? <h2>Envio Gratuito</h2> : null
@@ -22,7 +24,9 @@ function Favoritos() {
                     {
                     e.condition == "new" ? <h2>Nuevo</h2> : <h2>Usado</h2>
                     }
-                    <img src ={e.thumbnail} alt={e.title}/>
+                    <button className={EliminarItem}>
+                    Eliminar
+                  </button>
                 </div>) 
                 }
            </div>
