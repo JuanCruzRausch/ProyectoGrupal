03d07938a4f7dd9b618a4b3a4a29909995d6f725
MonzonJoaquin <84410085+MonzonJoaquin@users.noreply.github.com>
2022-07-02 11:40:09 -0300

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ function NavbarComponent(props) {
 
   useEffect(() => {
     user?dispatch(setUser(user)):null
-  },[dispatch])
+  },[setUser])
 
   const searchOnSubmit = (e) => {
     e.preventDefault();
