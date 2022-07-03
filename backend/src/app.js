f06294922d5a3b5594c03b9a4de3c4e2c0edf38f
MonzonJoaquin <84410085+MonzonJoaquin@users.noreply.github.com>
2022-07-03 14:30:05 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
 });
 
