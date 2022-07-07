e717a7ca819774c0c253542e9bf2b10226cfff42
Javierzv777 <63484661+Javierzv777@users.noreply.github.com>
2022-07-07 18:30:29 -0500

    merge commit (#230)

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -3,20 +3,34 @@ import React from 'react'
 import {useSelector} from "react-redux"
 import SellerProfile from '../Seller/SellerProfile';
 import {chat_header, chat_body, chat_footer} from "./Chat.module.css"
+import { useDispatch } from 'react-redux';
 
-export default function Chat({socket}) {
+export default function Chat({socket, _id}) {
     
+    const dispatch = useDispatch()
+    const recived = useSelector(state => state.interactionsReducer.chat)
     const [saludo, setSaludo] = React.useState({})
     const seller = useSelector(state => state.userReducer.seller)
     const mostrarComentarios = (e)=>{
         e.preventDefault()
         socket.emit("comentarios", saludo);
     };
-
+    const setRecived = (data) =>{
+        dispatch({type: "SET_CHAT", payload: data})
+    }
     React.useEffect(()=>{
         seller?setSaludo({
-            ...saludo, idSeller: seller?._id}):null
+            ...saludo, 
+            idSeller: seller?._id,
+            product_Id: _id
+        }):null
     },[seller])
+
+    React.useEffect(()=>{
+        socket.on("envio_front", (data)=>{
+            setRecived( data)
+        })
+    },[socket])
 
     const handleOnChange = (e) =>{
         
@@ -41,6 +55,7 @@ export default function Chat({socket}) {
                 {comentarios?._id===seller._id?<div className={seller_comentario}>{comentarios?.message}</div>:
                 <div className={cliente_comentario}>{comentarios?.message}</div>}
             </div>)}
+            {recived?.map(data=> <div><h1>{data.data}</h1></div>)}
         </div>
         <div className={chat_footer}>
             <form action="" onSubmit={(e)=> mostrarComentarios(e)}>
