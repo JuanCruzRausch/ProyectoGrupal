ff7c70b692e41850df3444f203dc9910d6ea27ee
Javier Zapata <javierzv777@gmail.com>
2022-07-08 18:07:33 -0500

    s

--- frontend/src/components/Seller/SellerProfile.jsx
+++ frontend/src/components/Seller/SellerProfile.jsx
@@ -122,7 +122,7 @@ export default function SellerProfile() {
   }
 
   const showPublication = (productId) =>{
-    dispatch(activePublication((productId, userState._id)))
+    dispatch(activePublication(productId, userState._id))
   }
 
   return (
