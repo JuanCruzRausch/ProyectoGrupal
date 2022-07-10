da5c4f0e1646a2451b150f747732540da21e9a83
cristhian0512 <cristhian.pinedab@gmail.com>
2022-07-10 08:53:13 -0500

    ruta-transaction-paypal-update

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -1,4 +1,5 @@
-//const Transaction = require ('../models/Transaction');
+const Transaction = require ('../models/Transaction');
+const CommonUser = require('../models/CommonUser');
 //const catchAsync = require('../utils/catchAsync');
 //const AppError = require('../utils/appError');
 const axios = require('axios');
@@ -7,84 +8,81 @@ const { PAYPAL_API, PAYPAL_API_CLIENT, PAYPAL_API_SECRET } = process.env;
 
 exports.createOrder = async(req,res,next)=>{
     try {
+        const {cartItem, shippingAddress} = req.body;
+        const {id} = req.params
+        
+        let itemsPaypal = []
+        for(let item of cartItem){
+            let itemObj = {
+                    id: item.product,
+                    name: item.title,
+                    description: item.title,
+                    sku: item.stock.stockTotal.toString(),
+                    unit_amount: {
+                         currency_code: "USD",
+                         value: item.price.toString()
+                    },
+                    tax: {
+                        currency_code: "USD",
+                        value: "0"
+                    },
+                    quantity: item.quantity.toString(),
+                    category: "PHYSICAL_GOODS"
+                }
+            itemsPaypal.push(itemObj);        
+        }
+        let total_value = 0;
+        for(let itemV of cartItem){
+            total_value=total_value+(itemV.price*itemV.quantity)
+        }
+        //Orden de compra que recibe Paypal
         const order = {
             intent: 'CAPTURE',
             purchase_units:[{
                 reference_id: "PUHF",
-                description: "Sporting Goods",
-
-                custom_id: "CUST-HighFashions",
-                soft_descriptor: "HighFashions",
+                description: "Orden de Pago MercadoEnanos",
+                custom_id: "CUST-MercadoEnanos",
+                soft_descriptor: "MercadoEnanos",
                 amount: {
                     currency_code: "USD",
-                    value: "230.00",
+                    value: total_value.toString(),
                     breakdown: {
                         item_total: {
                             currency_code: "USD",
-                            value: "180.00"
+                            value: total_value.toString()
                         },
                         shipping: {
                             currency_code: "USD",
-                            value: "30.00"
+                            value: "0.00"
                         },
                         handling: {
                             currency_code: "USD",
-                            value: "10.00"
+                            value: "0.00"
                         },
                         tax_total: {
                             currency_code: "USD",
-                            value: "20.00"
+                            value: "0.00"
                         },
                         shipping_discount: {
                             currency_code: "USD",
-                            value: "10"
+                            value: "0.00"
                         }
                     }
                 },
-                items: [{
-                    name: "T-Shirt",
-                    description: "Green XL",
-                    sku: "sku01",
-                    unit_amount: {
-                         currency_code: "USD",
-                         value: "90.00"
-                    },
-                    tax: {
-                        currency_code: "USD",
-                        value: "10.00"
-                    },
-                    quantity: "1",
-                    category: "PHYSICAL_GOODS"
-                },
-                    {
-                    name: "Shoes",
-                    description: "Running, Size 10.5",
-                    sku: "sku02",
-                    unit_amount: {
-                         currency_code: "USD",
-                         value: "45.00"
-                    },
-                    tax: {
-                        currency_code: "USD",
-                        value: "5.00"
-                    },
-                    quantity: "2",
-                    category: "PHYSICAL_GOODS"
-                }
-                ],
+                items: itemsPaypal,
                 shipping: {
-                    method: "United States Postal Service",
+                    method: "Delivery",
                     address: {
                         name: {
-                            full_name: "John",
-                            surname: "Doe"
+                            full_name: shippingAddress.fullname,
+                            surname: shippingAddress.fullname
                         },
-                        address_line_1: "123 Townsend St",
-                        address_line_2: "Floor 6",
-                        admin_area_2: "San Francisco",
-                        admin_area_1: "CA",
-                        postal_code: "94107",
-                        country_code: "US"
+                        address_line_1: shippingAddress.address,
+                        address_line_2: '-',
+                        admin_area_2: shippingAddress.city,
+                        admin_area_1: shippingAddress.country,
+                        postal_code: shippingAddress.postalCode,
+                        country_code: "AR"
                     }
                 }
             }],
@@ -99,7 +97,7 @@ exports.createOrder = async(req,res,next)=>{
         
         const params = new URLSearchParams();
         params.append('grant_type', 'client_credentials');
-
+        //Genero el access token
         const {data:{access_token}} = await axios.post('https://api-m.sandbox.paypal.com/v1/oauth2/token', params, {
             headers:{
                 'Content_Type':'application/x-www-form-urlencoded'
@@ -109,14 +107,27 @@ exports.createOrder = async(req,res,next)=>{
                 password: PAYPAL_API_SECRET
             }
         })
-        console.log('token', access_token)
+
+        // se manda info a paypal
         const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`, order, {
             headers:{
                 Authorization: `Bearer ${access_token}`
             }
         })
-        console.log('data la ptmreeeee', response.data);
-        res.json(response.data.links[1].href)
+
+        //--guardar en commonuser la orden compra
+        let products = []
+        cartItem.map(el=>products.push({
+            publicationId: el.product,
+            quantity: el.quantity
+        }));
+
+        let user = await CommonUser.findByIdAndUpdate(id,{purchase_order: {
+            products: products,
+            link: response.data.links[1].href
+        }})
+        
+        res.json(response.data.links[1].href) //-- devuelvo el link de pago
     } catch (error) {
         //console.log(error);
         next(error);
@@ -132,8 +143,12 @@ exports.captureOrder = async(req,res,next)=>{
             password: PAYPAL_API_SECRET
         }
     })
+
+    //---create de transactions
+
     res.json(response.data);
 }
 exports.cancelOrder = async(req,res,next)=>{
+
     res.json('hola')
 }
