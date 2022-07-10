a0225842d37877f4a29c5d93fa2875d606d9c345
MonzonJoaquin <84410085+MonzonJoaquin@users.noreply.github.com>
2022-07-10 10:49:00 -0300

    only admins

--- backend/src/routes/deleteUserRouter.js
+++ backend/src/routes/deleteUserRouter.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
 const deleteUserController = require('../controllers/deleteUserController');
+const {authorizeAccessToken} = require('../utils/authorizeAccessToken')
+const {roles} = require('../utils/roles')
 
 const deleteUserRouter = Router();
 //ESTA RUTA ES SEARCH AND DESTROY PARA EL ADMIN SOLAMENTE
-deleteUserRouter.post('/:id', deleteUserController.getPostAndDelete); 
+deleteUserRouter.post('/:id', authorizeAccessToken, roles.admin, deleteUserController.getPostAndDelete); 
 
 module.exports = deleteUserRouter;
