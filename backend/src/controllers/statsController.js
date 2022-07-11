0486b45226b91ee6649d4ad2ec45b3d4ecc8e38a
JMBuccolini <96688216+JMBuccolini@users.noreply.github.com>
2022-07-10 23:49:05 -0300

    Modificacion en stats getAllSales -> getAllActivePubs y sumado getTotalSales

--- backend/src/controllers/statsController.js
+++ backend/src/controllers/statsController.js
@@ -1,5 +1,7 @@
 const AppError = require('../utils/appError')
 const Seller = require('../models/Seller');
+const Transaction = require('../models/Transaction');
+const catchAsync = require('../utils/catchAsync')
 const {getAccessTokenAdmin, apiAuth0} = require('../utils/apiAdminAuth0')
 
 
@@ -29,7 +31,7 @@ exports.getListUsers = async(_, res, next) => {
     }
 }
 
-exports.getAllSales = async (_, res, next) => {
+exports.getAllActivePubs = async (_, res, next) => {
     try {
         let num = 0
         const listSeller = await Seller.find({})
@@ -42,4 +44,16 @@ exports.getAllSales = async (_, res, next) => {
         next(new AppError(error))
     }
 }
+
+exports.getTotalSales = catchAsync(async(_,res,next)=>{
+    const salesFinder = await Transaction.countDocuments({status:'fulfilled'})
+    console.log(salesFinder)
+    if(!salesFinder){
+        return next(new AppError('There are no fulfilled transactions', 404))
+    }
+    res.status(200).json({
+        status: 'success',
+        data:salesFinder
+    })
+})
 
