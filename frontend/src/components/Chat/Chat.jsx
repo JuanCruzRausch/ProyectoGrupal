056f57999b2cd674766f58632498b6be37a7ffea
Federico Carballo <77030730+FedeCarballo@users.noreply.github.com>
2022-07-11 09:55:22 -0300

    Work hard (#286)
    * css
    
    * funciona (por ahora)
    
    * ahora si

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -10,11 +10,15 @@ import {chat_header,
     PyR_content,
     PyR_content_Pregunta,
     PyR_content_Respuesta,} from "./Chat.module.css"
+import {
+    Dark_PyR 
+} from './DarkChat.module.css'
 import { useDispatch } from 'react-redux';
 
 
 export default function Chat({socket, _id, questions}) {
-    
+    const mode = useSelector((state)=> state.darkMode)
+    const { isdarkMode } = mode;
     const dispatch = useDispatch()
     const chat = useSelector(state => state.interactionsReducer.chat)
     const [message, setMessage] = React.useState({})
@@ -98,7 +102,7 @@ export default function Chat({socket, _id, questions}) {
 
     return (
     <div>
-         <div className={PyR_container}>
+         <div className={isdarkMode ? Dark_PyR : PyR_container}>
              <h1>preguntas y respuestas</h1>
              <hr />
         <div className={chat_footer}>
