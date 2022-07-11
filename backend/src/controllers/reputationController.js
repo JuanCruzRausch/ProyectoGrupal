903982710827ca83f9eed1e2dc123aee543ad7bb
JMBuccolini <96688216+JMBuccolini@users.noreply.github.com>
2022-07-11 18:27:04 -0300

    la reputation

--- backend/src/controllers/reputationController.js
+++ backend/src/controllers/reputationController.js
@@ -3,19 +3,20 @@ const PublicationTest = require('../models/PublicationTest');
 const catchAsync = require('../utils/catchAsync');
 
 exports.updateReputation = catchAsync(async(req,res,next) =>{
-    const {id} = req.params
+    const idseller = req.params.idseller
+    const idpubli = req.params.idpubli
     const {rep} = req.body // rating del vendedor
     const {rat} = req.body // rating de la publicación
 
-    const findSeller = await Seller.find({user: id});
+    const findSeller = await Seller.find({_id:idseller});
     if(!findSeller){
         return next(new AppError('There is not a seller with that id', 404))
     }
 
-    const sellerRating = await Seller.updateOne({user:id},{reputation:{total_votes: [...findSeller.reputation.total_votes,rep]}})
+    const sellerRating = await Seller.updateOne({_id:idseller},{reputation:{total_votes: [...findSeller.reputation.total_votes,rep]}})
 
-    const findPublication = await PublicationTest.find({seller: findSeller._id});
-    const publiRating = await PublicationTest.updateOne({seller: findSeller._id}, {rating:{total_votes:[...findPublication.rating.total_votes,rat]}})
+    const findPublication = await PublicationTest.find({_id: idpubli});
+    const publiRating = await PublicationTest.updateOne({_id: idpubli}, {rating:{total_votes:[...findPublication.rating.total_votes,rat]}})
 
     res.status(200).json({
         status:'success',
--- backend/src/routes/reputationRouter.js
+++ backend/src/routes/reputationRouter.js
@@ -3,6 +3,6 @@ const reputationController = require('../controllers/reputationController');
 
 const reputationRouter = Router();
 
-reputationRouter.patch('/', reputationController.updateReputation);
+reputationRouter.patch('/:idpubli/:idseller', reputationController.updateReputation);
 
 module.exports = reputationRouter;
