34aacb36e8946edb316be6f85ba05ff85074bd69
Javier Zapata <javierzv777@gmail.com>
2022-07-11 21:07:05 -0500

    subcategories

--- frontend/src/components/Subcategory/Subcategory.jsx
+++ frontend/src/components/Subcategory/Subcategory.jsx
@@ -1,16 +1,19 @@
 import {container} from "./Subcategory.module.css"
 import React from 'react'
 import { useSelector } from "react-redux"
-
+import { useDispatch } from 'react-redux'
 
 export default function Subcategory() {
 const categories = useSelector(state => state.productReducer.Categories)
 const category = useSelector(state => state.productReducer.category)
 const [categoryFound, setCategoryFound ]= React.useState({})
+const products = useSelector(state => state.productReducer.allProductCache)
+const dispatch = useDispatch()
 
-
-const subcategory = (e) => {
+const subcategory = (e, subcat) => {
   e.preventDefault()
+  const productSubcat =  products.filter(product => product.subCategory === subcat)
+  dispatch({type: "GET_PRODUCT", payload: [...productSubcat]})
 }
 React.useEffect(()=>{
   setCategoryFound({...categories?.find((cat => cat._id === category))})
@@ -22,7 +25,7 @@ React.useEffect(()=>{
       {categoryFound?.subcategories?.map(subcat=>(
   
         <div key={subcat._id}>
-          <a href="#" onClick={e=> subcategory(e)}>{subcat?.name}</a>
+          <a href="#" onClick={e=> subcategory(e, subcat._id)}>{subcat?.name}</a>
         </div>
   
       ))}
