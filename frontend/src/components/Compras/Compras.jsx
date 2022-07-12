6cd4ad60215750d9e6892dcc95d3cf156c8b5f36
Javierzv777 <63484661+Javierzv777@users.noreply.github.com>
2022-07-12 14:34:53 -0500

    Compra (#330)
    * first commit
    
    * tercer comit

--- frontend/src/components/Compras/Compras.jsx
+++ frontend/src/components/Compras/Compras.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { container } from './Compras.module.css'
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getPurchases } from '../../redux/actions/userAction'
+import { getPurchases, reject, arrived } from '../../redux/actions/userAction'
 import { Table }  from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 export default function Compras() {
@@ -11,7 +11,10 @@ export default function Compras() {
   const dispatch = useDispatch()
   useEffect(()=>{
     dispatch(getPurchases(user?._id))
-  },[])
+  },[user])
+
+
+
     return (
     <div className={container}>
         <Table>
@@ -22,9 +25,9 @@ export default function Compras() {
                     <th>PRODUCTO</th>
                     <th>PRECIO</th>
                     <th>CANTIDAD</th>
-                    <th>COMPRADOR</th>
-                    <th>GANANCIA</th>
+                    <th>MARCA</th>
                     <th>STATUS</th>
+                    <th>ACCIONES</th>
                     <th>FECHA</th>
                 </tr>
                 
@@ -33,17 +36,20 @@ export default function Compras() {
                  {purchases?.map(purchase =>(
                      <tr>
                         <td>{purchase._id}</td>
-                        <Link to={`/products/${purchase?.transaction?.publication?._id}`}><td >{purchase?.transaction?.publication?.title}</td></Link>
+                        {purchase?.transaction?.publication?.title?<Link to={`/products/${purchase?.transaction?.publication?._id}`}><td >{purchase?.transaction?.publication?.title}</td></Link>:
+                        <Link to={`/products/${purchase?.transaction?.publication?._id}`}><td >{purchase?.transaction?.publication?._id}</td></Link>}
                         <td>{purchase?.publication?.price}</td>
                         <td>{purchase?.transaction?.quantity}</td>
-                        <td>{purchase?.buyer?.name}</td>
-                        <td>{purchase?.transaction?.earnings?.user_earnings}</td>
+                        <td>{purchase?.transaction?.seller?.brand}</td>
                         <td>{purchase?.transaction?.status}</td>
+                        <td>
+                            {purchase?.transaction?.status==="pending"&&<button className="btn-success button" onClick={()=>dispatch(arrived(purchase._id))}>Confirmar</button>}
+                            {purchase?.transaction?.status==="pending"&&<button className="btn-danger button" onClick={()=>dispatch(reject(purchase._id))}>Cancelar</button>}
+                            {purchase?.transaction?.status==="fulfilled"&&<button className="btn-primary button" onClick={()=>{}}>Calificar</button>}
+                        </td>
                         <td>{purchase?.dateOfBuy?.substring(0, 10)}</td>
                     </tr>
                  ))}   
-                
-            
             </tbody>
         </Table>
     </div>
--- frontend/src/components/Transtaction/Transaction.jsx
+++ frontend/src/components/Transtaction/Transaction.jsx
@@ -16,6 +16,7 @@ useEffect(()=>{
           icon: "success",
         });
         dispatch(getTransaction(queries))
+        navigate("/")
 },[])
 navigate("/")
 
