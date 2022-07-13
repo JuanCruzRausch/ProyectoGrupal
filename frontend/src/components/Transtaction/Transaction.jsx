4204b89239435c3ba60b9ce0de4d22b403b16577
Javierzv777 <63484661+Javierzv777@users.noreply.github.com>
2022-07-13 08:29:25 -0500

    fixes (#356)

--- frontend/src/components/Transtaction/Transaction.jsx
+++ frontend/src/components/Transtaction/Transaction.jsx
@@ -4,24 +4,43 @@ import { useLocation } from 'react-router'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getTransaction } from '../../redux/actions/InteractionsActions'
+import { container_buttons, container } from './Transaction.module.css'
+import { Button } from 'react-bootstrap'
 
 export default function Transaction () {
     const queries = useLocation().search
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const transaction = useSelector(state => state.interactionsReducer.transaction)
+    const transaction = useSelector(state => state.interactionsReducer.transactionDetail)
 useEffect(()=>{
        
         dispatch(getTransaction(queries))
-        navigate("/")
+        // navigate("/")
 },[])
-navigate("/")
+useEffect(()=>{
+  if(transaction==="success"){
+    swal({
+      title: `Transacción exitosa`,
+      icon: 'success',
+    });
+    dispatch({type: 'SET_TRANSACTION_DETAIL', payload: "none"})
+  }
+},[transaction])
+
 
   return (
-    <div>
-        <button onClick={() => navigate("/")}>
-          Regresar 
-        </button>
+    <div className={container}>
+      <h1>Regresar a:</h1>
+      <div className={container_buttons}>
+        <br />
+        <br />
+        <Button onClick={() => navigate("/")}>
+          Home
+        </Button>
+        <Button onClick={() => navigate("/perfil/compras")}>
+          Historial de Compras
+        </Button>
+      </div>
     </div>
   )
 }
