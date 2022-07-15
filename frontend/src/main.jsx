50a5b2eb665da67c57d3eb0572f76ccf5670745f
Javierzv777 <63484661+Javierzv777@users.noreply.github.com>
2022-07-15 13:58:28 -0500

    Branck to deploy (#414)
    * seting transaction component
    
    * seting transaction component
    
    * seting transaction component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import { HelmetProvider } from 'react-helmet-async';
 import axios from 'axios';
 
-axios.defaults.baseURL =   "http://localhost:5050" ;
-// axios.defaults.baseURL =   "https://ml7e.herokuapp.com/" || "http://localhost:5050" ;
+// axios.defaults.baseURL =   "http://localhost:5050" ;
+axios.defaults.baseURL =   "https://ml7e.herokuapp.com/" || "http://localhost:5050" ;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
